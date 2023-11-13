3d4b1cdc20b3a2c7cb5e599c0cbc76871b05314c
YC.W <contact@wangyuchen.cn>
2023-11-13 23:40:25 +0800

    chore: support process.env. NODE_ENV

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,37 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
+import mockDevServerPlugin from "vite-plugin-mock-dev-server";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'GITHUB_PAGES' ? '/baota-dashboard/' : '',
+  base: process.env.NODE_ENV === "GITHUB_PAGES" ? "/baota-dashboard/" : "",
   plugins: [react(), mockDevServerPlugin()],
   server: {
     proxy: {
-      '^/api': { target: 'http://example.com' }
-    }
+      "^/api": { target: "http://example.com" },
+    },
+  },
+  define: {
+    "process.env": {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
       "@longzai-cn/baota-api-client": path.resolve(
         __dirname,
         "../packages/baota-api-client/src"
       ),
     },
   },
-  build:{
+  build: {
     rollupOptions: {
       output: {
         // manualChunks 配置
         manualChunks: {
           // 将 React 相关库打包成单独的 chunk 中
-          'react-vendor': ['react', 'react-dom'],
+          "react-vendor": ["react", "react-dom"],
           // 将组件库的代码打包
-          'library': ['antd'],
+          library: ["antd"],
         },
       },
-    }
-  }
+    },
+  },
 });
